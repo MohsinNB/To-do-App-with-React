c27ef99aed0d581a0d7e6ccd5ff097cd6c29740d
MohsinNB <mohammadmohsin28012005@gmail.com>
2024-08-21 12:33:57 +0600

    complete Ui part

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/AppName";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2";
+import "./App.css";
+function App() {
+  return (
+    <center className="todo-container">
+      {/* <AppName /> it's called self closing. */}
+      <AppName />
+
+      <div className="Item-container">
+        <AddTodo></AddTodo>
+        <TodoItem1></TodoItem1>
+        <TodoItem2></TodoItem2>
+      </div>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoItem2.jsx
@@ -0,0 +1,22 @@
+function TodoItem2() {
+  return (
+    <div class="container">
+      <div class="row kg-row">
+        <div class="col-5">
+          <h2>Go to college</h2>
+        </div>
+
+        <div class="col-4">
+          <h2>4/07/24</h2>
+        </div>
+        <div class="col-3">
+          <button type="button" class="btn btn-danger kg-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem2;
